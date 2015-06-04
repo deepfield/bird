32b4737d602fab7a6d00f8b87f1dc07427f7ca3d
Luke Carmichael <luke@deepfield.net>
2015-06-04 11:44:57 -0400

    do not truncate dumps, up constant size

--- build-deb.sh
+++ build-deb.sh
@@ -1,15 +1,24 @@
 #!/bin/bash
 
+V=$(cat VERSION)
+
 apt-get install build-essential autoconf checkinstall flex bison libncurses5-dev libreadline-dev git -y
 
+# build ipv6
+git clean -dxf
 autoconf
 ./configure --enable-ipv6
 make
 checkinstall --pkgversion=`cat VERSION` --pkgname=bird6
+mv bird6_$V-1_amd64.deb /tmp/
 
+# build ipv4
 git clean -dxf
-
 autoconf
 ./configure
 make
 checkinstall --pkgversion=`cat VERSION` --pkgname=bird
+
+mv /tmp/bird6_$V-1_amd64.deb .
+
+
